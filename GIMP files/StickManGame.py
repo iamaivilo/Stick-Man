2e55330a5d244b51ee62bbb2342a98984fe876a4
Shane <xianheng.ma@gmail.com>
2025-05-27 20:35:32 -0700

    I wrote some Coords I am bored hehehehe I can watch TV.

--- GIMP files/StickManGame.py
+++ GIMP files/StickManGame.py
@@ -2,6 +2,43 @@ from tkinter import *
 import random
 import time
 
+class Coords:
+    def __init__(self):
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+        
+def within_x (co1, co2):
+    if co1.x1 > co2.x1 and co1.x1 < co2.x2:
+        return True
+    elif co1.x2 > co2.x1 and co1.x2 < co2.x2:
+        return True
+    elif co2.x1 > co1.x1 and co1.x2 < co2.x2:
+        return True
+    elif co2.x2 > co1.x1 and co2.x2 < co1.x2:
+        return True
+    else : 
+        return False
+    
+def within_y (co1, co2):
+    if co1.y1 > co2.y1 and co1.y1 < co2.y2:
+        return True
+    elif co1.y2 > co2.y1 and co1.y2 < co2.y2:
+        return True
+    elif co2.y1 > co1.y1 and co1.y2 < co2.y2:
+        return True
+    elif co2.y2 > co1.y1 and co2.y2 < co1.y2:
+        return True
+    else : 
+        return False
+    
+def collided_left(co1,co2):
+    if within_y(co1,co2):
+        if co1.x1 >= co2.x1 and co1.x1 <= co2.x2:
+            return True
+    return False
+        
 class Game:
     def __init__(self):
         self.tk = Tk()
